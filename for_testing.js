00f12c633e0c6f0cd520b13e4e3178dc6171e8cb
polash@uni <musfiqpolash@gmail.com>
2023-05-24 15:05:56 +0200

    binding-cpp example

---
+++ for_testing.js
@@ -0,0 +1,16 @@
+// var opsys = process.platform;
+// if (opsys == "darwin") {
+//     opsys = "MacOS";
+// } else if (opsys == "win32" || opsys == "win64") {
+//     opsys = "Windows";
+// } else if (opsys == "linux") {
+//     opsys = "Linux";
+// }
+// console.log(opsys) // I don't know what linux is.
+
+const { autoDetect } = require('@serialport/bindings-cpp');
+const Binding = autoDetect();
+Binding.list().then(ports=>{
+    const port = ports.find(port => /arduino/i.test(port.manufacturer))
+    console.log(port.path);
+});
